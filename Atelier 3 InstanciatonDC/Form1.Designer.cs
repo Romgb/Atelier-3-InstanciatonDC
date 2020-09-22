3a58fde5521e9f1fb40989d2d3d0415b5a2aca8a
Romgb <romain.gilibert5@gmail.com>
2020-09-22 17:41:51 +0200

    Ajoutez des fichiers projet.

---
+++ Atelier 3 InstanciatonDC/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Atelier_3_InstanciatonDC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.reponseLignes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(385, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 358);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // reponseLignes
+            // 
+            this.reponseLignes.Location = new System.Drawing.Point(108, 127);
+            this.reponseLignes.Name = "reponseLignes";
+            this.reponseLignes.Size = new System.Drawing.Size(100, 20);
+            this.reponseLignes.TabIndex = 1;
+            this.reponseLignes.Click += new System.EventHandler(this.reponseLignes_TextChanged);
+            this.reponseLignes.TextChanged += new System.EventHandler(this.reponseLignes_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Combien de lignes voulez-vous dans votre damier?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reponseLignes);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox reponseLignes;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Atelier 3 InstanciatonDC/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atelier_3_InstanciatonDC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void reponseLignes_TextChanged(object sender, EventArgs e)
+        {
+
+            bool resultat = int.TryParse(reponseLignes.Text, out int result);
+
+            if (resultat == false)
+            {
+                MessageBox.Show("Veuillez entrer un chiffre!", "Erreur!",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);                              
+            }
+            else
+            {
+                groupBox1.Controls.Clear();
+                int x = 0;
+                int y = 0;
+                int taille = groupBox1.Width / result;
+
+                for (int j = 0; j <= result; j++)
+                {
+                    for (int i = 0; i <= result; i++)
+                    {
+                        Button bouton = new Button();
+                        bouton.Text = i.ToString();
+                        bouton.Size = new System.Drawing.Size(taille, taille);
+                        //bouton.Location = new Point(bouton.Width * i, y);
+                        bouton.Location = new Point(x, y);
+                        groupBox1.Controls.Add(bouton);
+                        x += taille;
+                        /*for (int j = 0; j <= result; j++)
+                        {
+                            Button bouton1 = new Button();
+                            bouton1.Text = j.ToString();
+                            bouton1.Size = new System.Drawing.Size((groupBox1.Height / result), (groupBox1.Height / result));
+                            bouton1.Location = new Point(x, bouton1.Height * j);
+                            groupBox1.Controls.Add(bouton1);
+                        }*/
+                    }
+                    y += taille;
+                    x = 0;
+                }
+            }
+           
+
+
+        }
+    }
+}
